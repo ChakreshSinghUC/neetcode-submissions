aedb67b8cd0200b3ed7a77d3b5b63635a93e7f95
Chakresh Singh <singhch@mail.uc.edu>
2026-07-22 00:33:20 -0400

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        for (auto ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else if (ch == ')' && (stk.empty() || stk.top() != '(')) {
+                return false;
+            } else if (ch == '}' && (stk.empty() || stk.top() != '{')) {
+                return false;
+            } else if (ch == ']' && (stk.empty() || stk.top() != '[')) {
+                return false;
+            } else {
+                stk.pop();
+            }
+        }
+
+        return stk.empty();
+    }
+};

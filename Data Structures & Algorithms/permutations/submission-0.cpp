7331c757c82e684e9661b4c219e4e05fb9865753
Chakresh Singh <singhch@mail.uc.edu>
2026-07-24 13:11:57 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    vector<vector<int>> ans;
+    void backtrack(vector<int>& nums, vector<int>& current, vector<bool>& used) {
+        if (current.size() == nums.size()) {
+            ans.push_back(current);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            used[i] = true;
+            current.push_back(nums[i]);
+            backtrack(nums, current, used);
+
+            used[i] = false;
+            current.pop_back();
+        }
+    }
+
+   public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> current;
+        vector<bool> used(nums.size(), false);
+        backtrack(nums, current, used);
+        return ans;
+    }
+};

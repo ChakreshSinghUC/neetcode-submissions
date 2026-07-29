0c6e6236166da6dd6fe07aefab558016bb2b4d82
Chakresh Singh <singhch@mail.uc.edu>
2026-07-28 21:27:06 -0400

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    int robLinear(vector<int> nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[nums.size() - 1];
+    }
+
+public:
+    int rob(vector<int>& nums) {
+
+        if (nums.size() == 0) {
+            return 0;
+        }
+
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+
+        vector<int> excludeLast(nums.begin(), nums.end() - 1);
+        vector<int> excludeFirst(nums.begin() + 1, nums.end());
+        return max(robLinear(excludeFirst),
+                   robLinear(excludeLast));
+    }
+};
